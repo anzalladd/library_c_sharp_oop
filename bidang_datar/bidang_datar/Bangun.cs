fd7fc6c90582e2a1f0d3a28911872c9511c3fc52
anzalladd <anzalladd@gmail.com>
2018-08-30 07:13:28 +0700

    first commit

---
+++ bidang_datar/bidang_datar/Bangun.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bidang_datar
+{
+    public class Bangun
+    {
+        public double persegi(double x)
+        {
+            return (x * x);
+
+            
+            
+        }
+        public double keliling_persegi(double x)
+        {
+            return (x * 4);
+
+
+        }
+        public double persegi_panjang(double x, double y)
+        {
+            return (x * y);
+        }
+        public double keliling_persegipanjang(double x, double y)
+        {
+            return (x * y * 2);
+
+
+
+        }
+        public double segitiga(double x, double y)
+        {
+            return (x * y / 2);
+        }
+        public double keliling_segitiga(double x, double y, double z)
+        {
+            return (x + y + z);
+
+
+        }
+    }
+}
---
+++ isibidang_datar/isibidang_datar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bidang_datar;
+
+namespace isibidang_datar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bangun bangun_datar = new Bangun();
+            Console.WriteLine("Luas dan Keliling");
+            Console.Write("Input x   : ");
+            double x = int.Parse(Console.ReadLine());
+            Console.Write("Input y   :");
+            double y = int.Parse(Console.ReadLine());
+            Console.Write("Input z   :");
+            double z = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Luas Persegi  =" + bangun_datar.persegi(x));
+            Console.WriteLine("");
+            Console.WriteLine("Keliling Persegi  =" + bangun_datar.keliling_persegi(x));
+            Console.WriteLine("");
+            Console.WriteLine("Luas Persegi Panjang  =" + bangun_datar.persegi_panjang(x,y));
+            Console.WriteLine("");
+            Console.WriteLine("Keliling Persegi Panjang  =" + bangun_datar.keliling_persegipanjang(x, y));
+            Console.WriteLine("");
+            Console.WriteLine("Luas Segitiga  =" + bangun_datar.segitiga(x, y));
+            Console.WriteLine("");
+            Console.WriteLine("Keliling Segitiga  =" + bangun_datar.keliling_segitiga(x,y,z));
+            Console.WriteLine("");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
